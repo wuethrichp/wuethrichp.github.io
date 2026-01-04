28a49cd2503125c8a824f86a8b1be667bf5ee2eb
wuethrichp <135245280+wuethrichp@users.noreply.github.com>
2026-01-04 21:43:13 +0100

    Add files via upload

---
+++ src/artifacts.js
@@ -0,0 +1,5 @@
+// --- START BUILD SCRIPT INJECTION ZONE ---
+// This array represents the dynamically generated list of artifacts.
+const ARTIFACT_DATA_GENERATED = [
+];
+// --- END BUILD SCRIPT INJECTION ZONE ---
